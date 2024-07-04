0ec54edf0b197557b5d8981d5634e0b4ee9d213a
alihajian <61647533+alii-hajian-e@users.noreply.github.com>
2024-07-04 17:45:19 +0330

    debug

--- lib/presention/controller/landing/view.dart
+++ lib/presention/controller/landing/view.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:go_router/go_router.dart';
+import 'dart:js' as js;
 
 import '../../component/item_landing_component/item_landing_component.dart';
 import '../../resources/assets_manager.dart';
@@ -77,8 +77,8 @@ class LandingPage extends StatelessWidget {
               ItemLanding(img: ImageAssets.sms, txt: 'پنل پیامکی',onTap: (){
                 logic.homeLogic.getPanelRoom(context);
                 logic.addMemberLogic.getUserList(context);
-                // js.context.callMethod('open', ['http://webapp.ibrokers.ir/navbarPage']);
-                GoRouter.of(context).go('/navbarPage');
+                js.context.callMethod('open', ['http://webapp.ibrokers.ir/navbarPage']);
+                // GoRouter.of(context).go('/navbarPage');
                 // Get.toNamed(Routes.navbarPage);SS
               })
             ],
